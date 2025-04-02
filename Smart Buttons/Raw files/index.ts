a27104865571bb19534905aea2ea03a1f54fcac2
ionavarga <168727770+ionavarga@users.noreply.github.com>
2025-04-02 11:09:05 +0200

    Create index.ts

---
+++ Smart Buttons/Raw files/index.ts
@@ -0,0 +1,136 @@
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
+import { SmartButton, SmartButtonProps } from "./SmartButton";
+import * as React from "react";
+
+export class SmartButtons implements ComponentFramework.ReactControl<IInputs, IOutputs> {
+    private notifyOutputChanged: () => void;
+    private _currentEntityId: string;
+    private _currentEntityName: string;
+    private _context: ComponentFramework.Context<IInputs>;
+    private _primaryName: string;
+    private _record: ComponentFramework.LookupValue;
+    /**
+     * Empty constructor.
+     */
+    constructor() {
+        // Empty
+    }
+
+    /**
+     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+     * Data-set values are not initialized here, use updateView.
+     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+     */
+    public init(
+        context: ComponentFramework.Context<IInputs>,
+        notifyOutputChanged: () => void,
+        state: ComponentFramework.Dictionary
+    ): void {
+        this.notifyOutputChanged = notifyOutputChanged;
+        const _ppUri = new URL(window.location.toString());
+        this._currentEntityId = _ppUri.searchParams.get("id") ?? "";
+        this._currentEntityName = _ppUri.searchParams.get("etn") ?? "";
+        this._context = context;
+        console.log("Fiddler")
+        console.log("Current Entity ID: " + this._currentEntityId);
+        
+    }
+
+    /**
+     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+     * @returns ReactElement root react element for the control
+     */
+    public updateView(context: ComponentFramework.Context<IInputs>): React.ReactElement {
+        this._primaryName = context.parameters.PrimaryName.raw ?? "No Name";
+        this._record = {
+            id: this._currentEntityId,
+            entityType: this._currentEntityName,
+            name: this._primaryName,
+        }
+        const props: SmartButtonProps = { 
+            _context: context,
+            onTask: this.handleTask.bind(this),
+            onEmail: this.handleEmail.bind(this),
+            onAppointment: this.handleAppointment.bind(this),
+            onPhoneCall: this.handlePhoneCall.bind(this),
+            onLetter: this.handleLetter.bind(this),
+            onFax: this.handleFax.bind(this)
+        };
+        return React.createElement(
+            SmartButton, props
+        );
+    }
+
+    public handleTask(): void {
+        const parameters = {}
+        this.handleActivity("task", parameters);
+    }
+
+    public handleEmail(): void {
+
+        const parameters = {}
+
+        this.handleActivity("email", parameters);
+    }
+
+    public handleAppointment(): void {
+        const parameters = {}
+        this.handleActivity("appointment", parameters);
+    }
+
+    public handlePhoneCall(): void {
+        const parameters = {}
+        this.handleActivity("phonecall", parameters);
+    }
+
+    public handleLetter(): void {
+        const parameters = {}
+        this.handleActivity("letter", parameters);
+    }
+
+    public handleFax(): void {
+        const parameters = {}
+        this.handleActivity("fax", parameters);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public handleActivity(entity:string, parameters: Record<string, any> | undefined): void {
+        console.log("Opening " + entity + " form for " + this._primaryName);
+        const record = this._record;
+
+        const pageInput: ComponentFramework.NavigationApi.EntityFormOptions = {
+            createFromEntity: record,
+            entityName: entity,
+        };
+        this._context.navigation.openForm(pageInput, parameters ).then(
+            function success() {
+                    // Run code on success
+                    return;
+            },
+            function error() {
+                    // Handle errors
+            }
+        ).catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    /**
+     * It is called by the framework prior to a control receiving new data.
+     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as "bound" or "output"
+     */
+    public getOutputs(): IOutputs {
+        return { };
+    }
+
+    /**
+     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+     * i.e. cancelling any pending remote calls, removing listeners, etc.
+     */
+    public destroy(): void {
+        // Add code to cleanup control if necessary
+    }
+}
